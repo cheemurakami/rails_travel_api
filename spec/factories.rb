551daddf7bef7f87a1d0029713c0a076df824cd4
Chisato Murakami <kiwipom1031@gmail.com>
2020-06-08 13:40:33 -0700

    add tests for post a review route

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory(:review) do
     author {Faker::FunnyName.name}
-    content {Faker::Hipsger.paragraph(sentence_count:2)}
+    content {Faker::Hipster.paragraph(sentence_count:2)}
     place {Faker::Address.country}
   end
 end
